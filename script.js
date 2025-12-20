df98534c81ea0b42b5738e18774528fb30f9fb07
Barnamoy004 <singhabarnamoy266@gmail.com>
2025-12-20 15:30:30 +0530

    Update

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".fade").forEach(e=>{
-setTimeout(()=>e.classList.add("show"),200)
-})
-
-document.querySelectorAll(".action").forEach(b=>{
-b.addEventListener("click",()=>{
-b.style.boxShadow="0 10px 25px rgba(0,0,0,.4)"
-})
-})
